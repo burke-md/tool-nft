92f762a188cc9d6d064d2d439ee94c08eec781ef
Michael Burke <michael.dburke56@gmail.com>
2022-04-13 14:15:56 -0400

    Implement pause/unpause w/ pre-transfer check.

--- contracts/Tool.sol
+++ contracts/Tool.sol
@@ -2,20 +2,36 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Tool is ERC721, Ownable {
+contract Tool is ERC721, Pausable, Ownable, ERC721Burnable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIdCounter;
 
   constructor() ERC721("Tool", "TOOL") {}
 
+  function pause() public onlyOwner {
+    _pause();
+  }
+
+  function unpause() public onlyOwner {
+    _unpause();
+  }
 //@dev handle minting cap
   function safeMint(address to) public onlyOwner {
     uint256 tokenId = _tokenIdCounter.current();
     _tokenIdCounter.increment();
     _safeMint(to, tokenId);
   }
+
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    whenNotPaused
+    override
+    {
+      super._beforeTokenTransfer(from, to, tokenId);
+    }
 }
