bdd41fe3a4a93d1b036942677e8c9bc160ccd553
Michael Burke <michael.dburke56@gmail.com>
2022-04-14 17:42:40 -0400

    Update config for public testnet.

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,14 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require('@nomiclabs/hardhat-ethers');
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
 
  module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
 };
